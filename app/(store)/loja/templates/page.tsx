de5747fd41151c03661972d4a64d152ac95e8bcd
aleksanderpalamar <apalamar@live.com>
2024-08-01 13:06:43 -0300

    update: updated length image avatar logo

--- app/(store)/loja/templates/page.tsx
+++ app/(store)/loja/templates/page.tsx
@@ -7,6 +7,8 @@ import products from "@/lib/products";
 import { TemplateCards } from "./_components/template-cards";
 
 const TemplatePage = () => {
+  const topProducts = products.slice(0, 4);
+
   return (
     <MaxContainer className="pb-12">
       <div className="flex items-center justify-between sticky top-0 z-10 py-6 bg-background">
@@ -19,7 +21,7 @@ const TemplatePage = () => {
         </p>
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {products.map((product) => (
+        {topProducts.map((product) => (
           <TemplateCards key={product.id} product={product} />          
         ))}
       </div>
