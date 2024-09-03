e3e77bf5da6df8ae5b79b2cf1c9c7b710d7ad5c5
aleksanderpalamar <apalamar@live.com>
2024-09-03 13:11:20 -0300

    update: social links.

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
-import { FaDev, FaInstagram, FaLinkedin, FaSteam, FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { FaDev, FaHackerrank, FaInstagram, FaLinkedin, FaThreads, FaYoutube } from "react-icons/fa6";
+import { SiBluesky } from "react-icons/si";
 
 const Footer = () => {
   return (
@@ -15,14 +16,14 @@ const Footer = () => {
         <Link href="https://www.linkedin.com/in/aleksanderpalamar" target="_blank" className="flex items-center gap-2">
           <FaLinkedin className="w-8 h-8 mb-4 border-[1px] border-zinc-700 rounded-md p-1 hover:bg-zinc-700" />
         </Link>
-        <Link href="" target="_blank" className="flex items-center gap-2">
-          <FaXTwitter className="w-8 h-8 mb-4 border-[1px] border-zinc-700 rounded-md p-1 hover:bg-zinc-700" />
+        <Link href="https://bsky.app/profile/aleksanderpalamar.dev" target="_blank" className="flex items-center gap-2">
+          <SiBluesky className="w-8 h-8 mb-4 border-[1px] border-zinc-700 rounded-md p-1 hover:bg-zinc-700" />
         </Link>
-        <Link href="" target="_blank" className="flex items-center gap-2">
-          <FaYoutube className="w-8 h-8 mb-4 border-[1px] border-zinc-700 rounded-md p-1 hover:bg-zinc-700" />
+        <Link href="https://www.threads.net/@palamar.dev" target="_blank" className="flex items-center gap-2">
+          <FaThreads className="w-8 h-8 mb-4 border-[1px] border-zinc-700 rounded-md p-1 hover:bg-zinc-700" />
         </Link>
-        <Link href="" target="_blank" className="flex items-center gap-2">
-          <FaInstagram className="w-8 h-8 mb-4 border-[1px] border-zinc-700 rounded-md p-1 hover:bg-zinc-700" />
+        <Link href="https://www.hackerrank.com/profile/palamar_dev" target="_blank" className="flex items-center gap-2">
+          <FaHackerrank className="w-8 h-8 mb-4 border-[1px] border-zinc-700 rounded-md p-1 hover:bg-zinc-700" />
         </Link>
       </div>
       <p className="text-center text-xs mt-4">© 2024 Aleksander Palamar. All rights reserved.</p>
