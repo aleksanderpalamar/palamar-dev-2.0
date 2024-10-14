c31ebe65bff1587be0c23c9363845e0fa6c1ee45
aleksanderpalamar <apalamar@live.com>
2024-10-14 09:24:51 -0300

    Add BlogSection, Mastodon link, and format ArticleCard props

--- components/shared/sections/BlogSection.tsx
+++ components/shared/sections/BlogSection.tsx
@@ -15,7 +15,12 @@ const BlogSection = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           {Array.from({ length: 2 }).map((_, _key) => (
             <ArticleCard 
-              key={_key} id={0} title="" description="" url="" social_image=""                           
+              key={_key} 
+              id={0} 
+              title="" 
+              description="" 
+              url="" 
+              social_image=""                           
             />
           ))}
         </div>
